931e62cdb2ae9c6b751bc83edf827d268a80a21b
github username <github email address>
2020-03-23 15:57:18 -0500

    Succesful js connection

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,7 +1,13 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
-const channels = require.context('.', true, /_channel\.js$/)
-channels.keys().forEach(channels)
+// const channels = require.context('.', true, /_channel\.js$/)
+// channels.keys().forEach(channels)
+
+
+const testDiv = document.createElement('div')
+
+const h1 = document.getElementById('h1')
+h1.innerText = "AYYYYYY"
 
 
