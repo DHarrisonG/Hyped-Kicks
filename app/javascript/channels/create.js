4cb1719a774c38ab3c27731d45f44ab6b9351986
github username <github email address>
2020-03-24 01:18:58 -0500

    added logo/Made a sneaker create page/ made a new user page

---
+++ app/javascript/channels/create.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const testDiv = document.createElement('div')
+
+    // function myFunction() {
+    //     var createdShoe = document.getElementById("createshoe").target;
+    // }
+
+    function createshoes(){
+        var makeShoes = document.getElementById('createshoe').reset();
+
+    }
+
+
+
+
+});
